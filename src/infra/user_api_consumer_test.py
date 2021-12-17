ef208420432ec4ca70e301ebe20a609a66753a25
elizandroDalastra <elizandro.dalastra@pampa.compasso>
2021-12-17 16:07:50 -0300

    test: Testting mock http request

--- src/infra/user_api_consumer_test.py
+++ src/infra/user_api_consumer_test.py
@@ -1,7 +1,10 @@
 from .user_api_consumer import UserApiConsumer
 
-def test_get_random_movie():
+def test_get_random_movie(requests_mock):
     ''' Test the get_random_iser method of UserApiConsumer class. '''    
+   
+    requests_mock.get('https://randomuser.me/api/', status_code=200, json={'results': [{'name': {'first': 'John', 'last': 'Doe'}}]})
+   
     user_api_consumer = UserApiConsumer()
     response = user_api_consumer.get_random_user()
 
