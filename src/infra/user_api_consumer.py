a77467715e63cef621ca6e07c3097d8cb7a9feda
elizandroDalastra <elizandro.dalastra@pampa.compasso>
2021-12-17 16:59:47 -0300

    feat: implementing error in user_api_consumer an testing with mocks

--- src/infra/user_api_consumer.py
+++ src/infra/user_api_consumer.py
@@ -1,26 +1,42 @@
-from typing import Type
+from typing import Tuple, Type
+from collections import namedtuple
 import requests
 from requests import Request
-from collections import namedtuple
+from src.errors import HttpRequestError
 
 class UserApiConsumer:
+
+    '''
+        Class to consumer the user api.
+    '''
 
     def __init__(self) -> None:
         self.get_random_user_response = namedtuple('get_random_user_response', 'status_code request response')
 
-    def get_random_user(self) -> any:
+    def get_random_user(self) -> Tuple[int, Request, dict]:
+        '''
+            Get a random user from the user api.
+            :return - Tuple with the status code, request and response.
+        '''
+
         req = requests.Request(
             method='GET', 
             url='https://randomuser.me/api/'
         )
         req_prepered = req.prepare()
         response = self.__send_http_request(req_prepered)
+        status_code = response.status_code
 
-        return self.get_random_user_response(
-            status_code=response.status_code,
-            request=req,
-            response=response.json()
-        )
+        if status_code >= 200 and status_code < 300:
+            return self.get_random_user_response(
+                status_code=status_code,
+                request=req,
+                response=response.json()
+            )
+        else:
+            raise HttpRequestError(
+                message=response.json['detail'], status_code=status_code
+            )
 
     @classmethod
     def __send_http_request(cls, req_prepered: Type[Request]) -> any:
--- src/infra/user_api_consumer_test.py
+++ src/infra/user_api_consumer_test.py
@@ -1,9 +1,10 @@
+from src.errors import HttpRequestError
 from .user_api_consumer import UserApiConsumer
 
-def test_get_random_movie():
+def test_get_random_movie(requests_mock):
     ''' Test the get_random_iser method of UserApiConsumer class. '''    
    
-    # requests_mock.get('https://randomuser.me/api/', status_code=200, json={'results': [{'name': {'first': 'John', 'last': 'Doe'}}]})
+    requests_mock.get('https://randomuser.me/api/', status_code=200, json={'results': [{'name': {'first': 'John', 'last': 'Doe'}}]})
    
     user_api_consumer = UserApiConsumer()
     get_random_user = user_api_consumer.get_random_user()
@@ -12,4 +13,21 @@ def test_get_random_movie():
     assert get_random_user.request.url == 'https://randomuser.me/api/'
     assert get_random_user.status_code == 200
     assert isinstance(get_random_user.response['results'], list)
+
+def test_get_user_http_error(requests_mock):
+    ''' Test the get_random_iser method of UserApiConsumer class. '''
+
+    requests_mock.get('https://randomuser.me/api/', status_code=200, json={'detail': 'something'})
+
+
+    user_api_consumer = UserApiConsumer()
+
+    try:
+        user_api_consumer.get_random_user()
+        assert True is True
+    except HttpRequestError as error:
+        assert error.message is not None
+        assert error.status_code is not None
+        print(error.message)
+        print(type(error))
 
