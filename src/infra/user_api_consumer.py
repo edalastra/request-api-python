3d330f737680baacc6af633952c464fb4279781a
elizandroDalastra <elizandro.dalastra@pampa.compasso>
2021-12-17 16:34:17 -0300

    test: Create get user test

--- src/infra/user_api_consumer.py
+++ src/infra/user_api_consumer.py
@@ -1,10 +1,35 @@
+from typing import Type
 import requests
+from requests import Request
+from collections import namedtuple
 
 class UserApiConsumer:
 
-    @classmethod
-    def get_random_user(self) -> any:
-       
-        response = requests.get('https://randomuser.me/api/')
+    def __init__(self) -> None:
+        self.get_random_user_response = namedtuple('get_random_user_response', 'status_code request response')
 
-        return response.json()
+    def get_random_user(self) -> any:
+        req = requests.Request(
+            method='GET', 
+            url='https://randomuser.me/api/'
+        )
+        req_prepered = req.prepare()
+        response = self.__send_http_request(req_prepered)
+
+        return self.get_random_user_response(
+            status_code=response.status_code,
+            request=req,
+            response=response.json()
+        )
+
+    @classmethod
+    def __send_http_request(cls, req_prepered: Type[Request]) -> any:
+        '''
+            Send the request to the API and return the response.
+            :param - req_prepered: The request object that has been prepared.
+            :response - Http response raw
+        '''
+
+        http_session = requests.Session()
+        response = http_session.send(req_prepered)
+        return response
--- src/infra/user_api_consumer_test.py
+++ src/infra/user_api_consumer_test.py
@@ -1,11 +1,15 @@
 from .user_api_consumer import UserApiConsumer
 
-def test_get_random_movie(requests_mock):
+def test_get_random_movie():
     ''' Test the get_random_iser method of UserApiConsumer class. '''    
    
-    requests_mock.get('https://randomuser.me/api/', status_code=200, json={'results': [{'name': {'first': 'John', 'last': 'Doe'}}]})
+    # requests_mock.get('https://randomuser.me/api/', status_code=200, json={'results': [{'name': {'first': 'John', 'last': 'Doe'}}]})
    
     user_api_consumer = UserApiConsumer()
-    response = user_api_consumer.get_random_user()
+    get_random_user = user_api_consumer.get_random_user()
 
-    return print(response)
+    assert get_random_user.request.method == 'GET'
+    assert get_random_user.request.url == 'https://randomuser.me/api/'
+    assert get_random_user.status_code == 200
+    assert isinstance(get_random_user.response['results'], list)
+
